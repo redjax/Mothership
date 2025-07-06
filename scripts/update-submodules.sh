78a795e42e6d57c5ffb69ecac1c8b622261237fa
redjax <5534031+redjax@users.noreply.github.com>
2025-07-05 22:12:23 -0400

    fix(submodules): Add newline print above exits

--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -10,6 +10,7 @@ echo ""
 
 git pull --recurse-submodules
 if [ $? -ne 0 ]; then
+  echo ""
   echo "Failed to pull submodules"
   exit 1
 fi
@@ -17,9 +18,11 @@ fi
 echo "Updating submodules recursively"
 git submodule update --init --recursive --remote
 if [ $? -ne 0 ]; then
+  echo ""
   echo "Failed to update submodules"
   exit 1
 fi
 
+echo ""
 echo "Updated submodules"
 exit 0
